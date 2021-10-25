f025ce4de9488525d597b58018a2d5055e19a68e
kinglinz <446908241@qq.com>
2021-10-25 18:21:50 +0800

    提交

--- app/admin/controller/CoursePlan.php
+++ app/admin/controller/CoursePlan.php
@@ -4,28 +4,94 @@ namespace app\admin\controller;
 
 
 use app\admin\model\CoursePlan as CoursePlanModel;
+use app\admin\model\Course as CourseModel;
 use think\Request;
 
 class CoursePlan extends Permissions
 {
     public function index()
     {
-            $model = new CoursePlanModel();
-            $data = $model->select();
-            $this->assign("course",$data);
-            return $this->fetch();
+        $model = new CoursePlanModel();
+        $data = $model->select();
+        $this->assign("course", $data);
+        return $this->fetch();
     }
 
-    public function publish(Request $request){
-        $id = $request->has('id') ? $request->param('id',0,'intval') : 0;
-        if($id > 0){
+    public function publish(Request $request)
+    {
 
-        }else{
-            if($request->isPost()){
+        $id = $request->has('id') ? $request->param('id', 0, 'intval') : 0;
+        if ($id > 0) {
+            if ($request->isPost()) {
+                $post = $request->post();
+                $validate = new  \think\Validate([
+                    ['title','require','标题不能为空'],
+                    ['class','require','班级不能为空'],
+                    ['course_id','require','课程分类不能为空'],
+                    ['location','require','位置不能为空'],
+                    ['class_hour','require','课时不能为空'],
+                    ['start_time','require','时间不能为空'],
+                ]);
 
-            }else{
+                if(!$validate->check($post)){
+                    return $this->error($validate->getError());
+                }
+                $model = new CoursePlanModel();
+                if($model->get($id)->allowField(true)->save($post)){
+                    return $this->success('修改成功','admin\course_plan\index');
+                }else{
+                    return $this->error('修改失败','admin\course_plan\index');
+                }
+            } else {
+                $cmodel = new CourseModel();
+                $pmodel = new CoursePlanModel();
+                $plan = $pmodel->get($id);
+                $data = $cmodel->where("is_online",0)->select();
+                $this->assign("info",$plan);
+                $this->assign("cate",$data);
+                dump($plan['start_time']);
                 return $this->fetch();
             }
+        } else {
+            if ($request->isPost()) {
+                $post = $request->post();
+                $validate = new  \think\Validate([
+                    ['title','require','标题不能为空'],
+                    ['class','require','班级不能为空'],
+                    ['course_id','require','课程分类不能为空'],
+                    ['location','require','位置不能为空'],
+                    ['class_hour','require','课时不能为空'],
+                    ['start_time','require','时间不能为空'],
+                ]);
+
+                if(!$validate->check($post)){
+                    return $this->error($validate->getError());
+                }
+                $model = new CoursePlanModel();
+                if($model->allowField(true)->save($post)){
+                    return $this->success('添加成功','admin\course_plan\index');
+                }else{
+                    return $this->error('添加失败','admin\course_plan\index');
+                }
+            } else {
+                $cmodel = new CourseModel();
+                $pmodel = new CoursePlanModel();
+                $plan = $pmodel->get($id);
+                $data = $cmodel->where("is_online",0)->select();
+                $this->assign("info",$plan);
+                $this->assign("cate",$data);
+                return $this->fetch();
+            }
+        }
+    }
+
+    public function delete(Request $request){
+        $id = $request->param('id');
+        $model = new CoursePlanModel();
+        if($model->get($id)->delete()){
+            return $this->success('删除成功','admin\course_plan\index');
+        }else{
+            return $this->error('删除失败 ','admin\course_plan\index');
         }
     }
 }
--- app/admin/model/CoursePlan.php
+++ app/admin/model/CoursePlan.php
@@ -4,5 +4,11 @@
     use think\Model;
 
     class CoursePlan extends Model{
+        protected function setStartTimeAttr($value){
+            return strtotime($value);
+        }
 
+        protected function getStartTimeAttr($value){
+            return date('Y-m-d',$value);
+        }
     }
