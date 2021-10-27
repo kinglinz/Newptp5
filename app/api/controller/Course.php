5f4c951b91645df05a9bb2df1462b0b329ba804c
kinglinz <446908241@qq.com>
2021-10-27 13:45:34 +0800

    202110271345xcx

--- app/api/controller/Course.php
+++ app/api/controller/Course.php
@@ -7,6 +7,7 @@ use app\admin\model\Course as courseModel;
 use app\admin\model\CourseInfo;
 use app\admin\model\BuyCourse as BuyPmodel;
 use app\admin\model\CourseCate as CourseCateModel;
+use app\admin\model\Learn as LearnModel;
 use think\Db;
 
 
@@ -21,7 +22,7 @@ class Course extends Login
         $cm = new courseModel();
         $data = array();
         $catedata = $coursecate->select();
-        $cmdata = $cm->where('is_online', 1)->select();
+        $cmdata = $cm->select();
         for ($i = 0; $i < count($catedata); $i++) {
             for ($j = 0; $j < count($cmdata); $j++) {
                 if ($cmdata[$j]['cate_id'] == $catedata[$i]['id']) {
@@ -32,7 +33,7 @@ class Course extends Login
         if (!empty($data)) {
             return tojson($data);
         } else {
-            return json(['code' => -1, 'msg' => '没有请求的数据']);
+            return tojson();
         }
 
         //return json(collection($c->all())->toArray());
@@ -42,34 +43,61 @@ class Course extends Login
     public function getCourseInfoList()
     {
         $cid = $this->request->get('id');
+        $uid = 98;
         $cmodel = new courseModel();
-        $descrption = $cmodel->field('descrption')->where('id', $cid)->find();
+        $d = $cmodel->get($cid);
         $infoModel = new CourseInfo();
         $data = $infoModel->where('course_id', $cid)->order('name')->select();
-        $data['descrption'] = $descrption;
+        
+       //dump($d);die;
         if(!empty($data)){
-            return tojson($data);
+            $data['descrption']  = $d['descrption'];
+            if($d['is_toll'] == 1){
+                return $this->is_toll($data,$uid,$cid);
+            }else{
+                return "dddddd";
+            }
         }else{
-            return tojson();
+            return '没有此课程';
         }
     }
 
 
     public function getcourse()
     {
-        $cid = $this->request->get('id');
+        $cid = $this->request->get('cid');
+        $uid = 98;
         $infoModel = new CourseInfo();
-        $data = $infoModel->where('id', $cid)->find();
-        return json($data);
+        $learn = new LearnModel();
+
+        $ret = $learn->field('id')->where('user_id',$uid)->where('course_info_id',$cid)->find();
+        //return($ret);
+        if(empty($ret)){
+            $learn->course_info_id = $cid;
+            $learn->user_id = $uid;
+            $learn->allowField(true)->save();
+        }
+
+        $data = $infoModel->get($cid);
+          
+        if(!empty($data)){
+            if($data['is_toll'] == 1){
+                return $this->is_toll($data,$uid);
+            }else{
+                return tojson();
+            }
+        }else{
+            return tojson();
+        }
     }
 
 
-    //获取培训列表
-    public function getReal(){
-        $coursemodel = new courseModel();
-        $data = $coursemodel->where('is_online',0)->select();
-        return json($data);
-    }
+    // //获取培训列表
+    // public function getReal(){
+    //     $coursemodel = new courseModel();
+    //     $data = $coursemodel->where('is_online',0)->select();
+    //     return json($data);
+    // }
 
     //培训报名
     public function apply()
@@ -133,7 +161,13 @@ class Course extends Login
     }
 
 
-    public function test()
+    public function is_toll($data,$uid,$cid="")
     {
+        $retsult = Db::name('buy_course')->field('id')->where('user_id',$uid)->where('course_id',$cid)->find();
+        if(!empty($retsult)){
+            return json($data);
+        }else{
+            return "需要购买";
+        }
     }
 }
--- app/common.php
+++ app/common.php
@@ -149,3 +149,19 @@ function hide_phone($str){
     $resstr = substr_replace($str,'****',3,4);  
     return $resstr;  
 }
+
+
+
+function tojson($data="",$code='',$msg=''){
+    if(empty($data)){
+        return json([
+            'code' => -1,
+             'msg' => '请求参数错误'
+        ]); 
+    }
+    return json([
+        'code' => 0,
+        'msg' => $msg,
+        'data' => $data
+    ]);
+}
